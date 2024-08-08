a726010d926a7c406758539346b19bcfda02744d
BugWithMind <kashin2329@gmail.com>
2024-08-08 18:45:46 +0300

    fix catch and connection name

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -17,54 +17,54 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " ( " +
                     "id BIGINT primary key auto_increment," +
                     "name varchar(40)," +
                     "lastname varchar(40)," +
                     "age TINYINT DEFAULT 18 CHECK(Age > 0 AND Age < 100))";
-            conn.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "DROP TABLE IF EXISTS " + tableName;
-            conn.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "INSERT INTO " + tableName + " (name, lastname, age) SELECT "
                     + "'" + name + "',"
                     + "'" + lastName + "',"
                     + age;
-            conn.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
             System.out.println("User с именем — " + name + " добавлен в базу данных");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "DELETE FROM " + tableName + " WHERE id = " + id;
-            conn.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "SELECT * FROM " + tableName;
-            ResultSet rs = conn.createStatement().executeQuery(sql);
+            ResultSet rs = connection.createStatement().executeQuery(sql);
             while (rs.next()) {
                 User user = new User(rs.getString("name"),
                         rs.getString("lastname"),
@@ -73,17 +73,17 @@ public class UserDaoJDBCImpl implements UserDao {
                 users.add(user);
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         return users;
     }
 
     public void cleanUsersTable() {
-        try (Connection conn = Util.getMySQLConnection()) {
+        try (Connection connection = Util.getMySQLConnection()) {
             String sql = "TRUNCATE TABLE " + tableName;
-            conn.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
